e31bf90965d40c9414ae94defbee9f9200fe86fd
Bankovskiy <bannkovskiy@gmail.com>
2023-11-02 17:26:05 +0200

    (added) collision detection

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,18 +29,29 @@
         private void InitializeComponent()
         {
             hero = new PictureBox();
+            food = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)hero).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)food).BeginInit();
             SuspendLayout();
             // 
             // hero
             // 
             hero.BackColor = Color.LightGreen;
-            hero.Location = new Point(517, 281);
+            hero.Location = new Point(566, 429);
             hero.Name = "hero";
             hero.Size = new Size(100, 100);
             hero.TabIndex = 0;
             hero.TabStop = false;
             hero.Click += hero_Click;
+            // 
+            // food
+            // 
+            food.BackColor = Color.FromArgb(255, 255, 128);
+            food.Location = new Point(591, 187);
+            food.Name = "food";
+            food.Size = new Size(50, 50);
+            food.TabIndex = 1;
+            food.TabStop = false;
             // 
             // Form1
             // 
@@ -48,17 +59,20 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Gray;
             ClientSize = new Size(1162, 694);
+            Controls.Add(food);
             Controls.Add(hero);
             Name = "Form1";
             Text = "Form1";
             KeyDown += Form1_KeyDown;
             KeyPress += Form1_KeyPress;
             ((System.ComponentModel.ISupportInitialize)hero).EndInit();
+            ((System.ComponentModel.ISupportInitialize)food).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private PictureBox hero;
+        private PictureBox food;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -21,7 +21,7 @@ namespace WASD_controls_app
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.A)
+            if (e.KeyCode == Keys.A)
             {
                 hero.Left -= 10;
             }
@@ -29,14 +29,25 @@ namespace WASD_controls_app
             {
                 hero.Left += 10;
             }
-            else if(e.KeyCode == Keys.W)
+            else if (e.KeyCode == Keys.W)
             {
                 hero.Top -= 10;
             }
-            else if(e.KeyCode == Keys.S)
+            else if (e.KeyCode == Keys.S)
             {
                 hero.Top += 10;
             }
+
+            CollisionCheck();
         }
+
+        private void CollisionCheck()
+        {
+            if(hero.Bounds.IntersectsWith(food.Bounds))
+            {
+                food.Visible = false;
+            }
+        }
+
     }
 }
