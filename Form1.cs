6a95e65a4faa4fc9bd003e0b44cd87430b5ab842
Bankovskiy <bannkovskiy@gmail.com>
2023-11-02 16:54:40 +0200

    + simple WASD control

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,37 @@ namespace WASD_controls_app
         {
             InitializeComponent();
         }
+
+        private void hero_Click(object sender, EventArgs e)
+        {
+            int speed = 30;
+            hero.Left += speed;
+            hero.Top += speed;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //MessageBox.Show($"You have pressed {e.KeyChar}");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.A)
+            {
+                hero.Left -= 10;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                hero.Left += 10;
+            }
+            else if(e.KeyCode == Keys.W)
+            {
+                hero.Top -= 10;
+            }
+            else if(e.KeyCode == Keys.S)
+            {
+                hero.Top += 10;
+            }
+        }
     }
 }
