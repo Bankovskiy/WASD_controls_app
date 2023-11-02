ff91b4f191ca1f76a51775bf03277399756c116d
Bankovskiy <bannkovskiy@gmail.com>
2023-11-02 17:44:26 +0200

    (added) refactured the code

--- Form1.cs
+++ Form1.cs
@@ -21,33 +21,36 @@ namespace WASD_controls_app
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.A)
-            {
-                hero.Left -= 10;
-            }
-            else if (e.KeyCode == Keys.D)
-            {
-                hero.Left += 10;
-            }
-            else if (e.KeyCode == Keys.W)
-            {
-                hero.Top -= 10;
-            }
-            else if (e.KeyCode == Keys.S)
-            {
-                hero.Top += 10;
-            }
-
+            MoveTheHero(e.KeyCode);
             CollisionCheck();
         }
 
         private void CollisionCheck()
         {
-            if(hero.Bounds.IntersectsWith(food.Bounds))
+            if (hero.Bounds.IntersectsWith(food.Bounds))
             {
                 food.Visible = false;
             }
         }
 
+        private void MoveTheHero(Keys keyCode)
+        {
+            if (keyCode == Keys.A)
+            {
+                hero.Left -= 10;
+            }
+            else if (keyCode == Keys.D)
+            {
+                hero.Left += 10;
+            }
+            else if (keyCode == Keys.W)
+            {
+                hero.Top -= 10;
+            }
+            else if (keyCode == Keys.S)
+            {
+                hero.Top += 10;
+            }
+        }
     }
 }
